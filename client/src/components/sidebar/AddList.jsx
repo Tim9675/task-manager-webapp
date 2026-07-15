8ba0133334442b41f748674d2358d621637d8be8
Tim9675 <tmmagno9675@gmail.com>
2026-07-15 17:52:15 +0800

    feat: replace h3 with span in AddList and AddTag components for improved semantic structure

--- client/src/components/sidebar/AddList.jsx
+++ client/src/components/sidebar/AddList.jsx
@@ -4,11 +4,11 @@ function AddList({ onOpen }) {
   return (
     <button
       type="button"
-      className="flex w-full cursor-pointer items-center rounded-md px-3 hover:bg-[#ebebeb] md:h-9"
       onClick={onOpen}
+      className="flex w-full cursor-pointer items-center rounded-md px-3 hover:bg-[#ebebeb] md:h-9"
     >
       <Plus color={"#7c7c7c"} size={16} strokeWidth={4} />
-      <h3 className="ms-3.5">Add New List</h3>
+      <span className="ms-3.5">Add New List</span>
     </button>
   );
 }
