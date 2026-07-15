974e02214be6286e405d15b951a889cb8c9c17ed
Tim9675 <tmmagno9675@gmail.com>
2026-07-15 16:24:39 +0800

    feat: refactor SidebarItem component for improved readability and structure

--- client/src/components/sidebar/SidebarItem.jsx
+++ client/src/components/sidebar/SidebarItem.jsx
@@ -1,71 +1,60 @@
+import { useTasks } from "../../contexts/TasksContext";
 import { useDisplay } from "../../contexts/DisplayContext";
-
 import TaskSidebarItem from "./TaskSidebarItem";
 import ListSidebarItem from "./ListSidebarItem";
 import TagSidebarItem from "./TagSidebarItem";
-import { useTasks } from "../../contexts/TasksContext";
 
 function SidebarItem({ nav, type }) {
+  const { closeTask } = useTasks();
   const { activeView, setActiveView, isSearching, setIsSearching } =
     useDisplay();
 
-  const { closeTask } = useTasks();
-
   const viewTitle = nav.title.toLowerCase();
-  const shouldCloseTask =
-    viewTitle !== "today" &&
-    viewTitle !== "upcoming" &&
-    viewTitle !== "all tasks";
+  const shouldCloseTask = nav.count == null;
 
-  function renderItem() {
-    switch (type) {
-      case "tasks":
-        return (
-          <TaskSidebarItem
-            nav={nav}
-            activeView={activeView}
-            isSearching={isSearching}
-            onDisplayChange={() => {
-              setIsSearching(false);
-              if (shouldCloseTask) closeTask();
-
-              setActiveView({
-                type: viewTitle.replaceAll(" ", ""),
-              });
-            }}
-          />
-        );
-      case "lists":
-        return (
-          <ListSidebarItem
-            nav={nav}
-            activeView={activeView}
-            setActiveView={setActiveView}
-            isSearching={isSearching}
-            onDisplayChange={() => {
-              setIsSearching(false);
-              setActiveView({ type: "list", id: nav._id });
-            }}
-          />
-        );
-      case "tags":
-        return (
-          <TagSidebarItem
-            nav={nav}
-            activeView={activeView}
-            setActiveView={setActiveView}
-            isSearching={isSearching}
-            onDisplayChange={() => {
-              setIsSearching(false);
-              setActiveView({ type: "tag", id: nav._id });
-            }}
-          />
-        );
-      default:
-        return null;
-    }
+  switch (type) {
+    case "tasks":
+      return (
+        <TaskSidebarItem
+          nav={nav}
+          activeView={activeView}
+          isSearching={isSearching}
+          onDisplayChange={() => {
+            setIsSearching(false);
+            if (shouldCloseTask) closeTask();
+            setActiveView({ type: viewTitle.replaceAll(" ", "") });
+          }}
+        />
+      );
+    case "lists":
+      return (
+        <ListSidebarItem
+          nav={nav}
+          activeView={activeView}
+          setActiveView={setActiveView}
+          isSearching={isSearching}
+          onDisplayChange={() => {
+            setIsSearching(false);
+            setActiveView({ type: "list", id: nav._id });
+          }}
+        />
+      );
+    case "tags":
+      return (
+        <TagSidebarItem
+          nav={nav}
+          activeView={activeView}
+          setActiveView={setActiveView}
+          isSearching={isSearching}
+          onDisplayChange={() => {
+            setIsSearching(false);
+            setActiveView({ type: "tag", id: nav._id });
+          }}
+        />
+      );
+    default:
+      return null;
   }
-  return renderItem();
 }
 
 export default SidebarItem;
